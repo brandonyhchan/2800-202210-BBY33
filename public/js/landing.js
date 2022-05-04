fcdaad234e3fc24691e9d5ea91f3a382348b2ea0
Stanley Chow <sjchow97@gmail.com>
2022-05-04 11:50:18 -0700

    Changed color scheme of landing page

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,6 +1,6 @@
 var images = ['/img/hero1.jpg', '/img/hero2.jpg', '/img/hero3.jpg', '/img/hero4.jpg'];
 var i = 0;
-var interval = 4000;
+var interval = 2000;
 
 /**
  * Creates a sliding image gallery that automatically updates every specified interval.
