c0cdee9e52c2c72f65c8cec5822103a1a1cb529c
Brandon Chan <brandonchan45@gmail.com>
2022-05-12 22:30:43 -0700

    Updates to map.html styling for both mobile and desktop view

--- public/js/purchase.js
+++ public/js/purchase.js
@@ -46,7 +46,7 @@ function getPackage() {
                             </div><div class='pImage'><img width='100' height='100' src="${row.package_image}">
                             </div><div class='price'> $${row.package_price} 
                             </div><div class='description'>${row.description_of_package}
-                            </div><div><button type='button' class='packages' id='${row.package_id}'>Add</button></div></div><button type='button' class='packagesDisplay' id='${row.package_name}'>viewMore</button></div></div><br>`);
+                            </div><div><button type='button' class='packages' id='${row.package_id}'>Add</button><button type='button' class='packagesDisplay' id='${row.package_name}'>More info</button></div></div></div></div>`);
                     }
                     document.getElementById("cart").innerHTML = str;
                 }
